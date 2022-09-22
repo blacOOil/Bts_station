81362ad1c875ff634eebe441633e9f789c09d99c
blacOOil <111561598+blacOOil@users.noreply.github.com>
2022-09-22 15:13:31 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Programe{
+    static void Main(string[] args ){
+        int answer;
+        Console.WriteLine("Please input a number of station: ");
+        answer = int.Parse(Console.ReadLine());
+        
+           if (answer == 1)
+           {
+            Console.WriteLine("Cost (THB) : 16");
+           }
+        if (answer == 2 )
+        {
+            Console.WriteLine("Cost (THB): 23");
+        }
+        if (answer == 3 )
+        {
+            Console.WriteLine("Cost (THB): 26");
+        }
+        if (answer == 4 )
+        {
+            Console.WriteLine("Cost (THB): 30");
+        }
+        if (answer == 5 )
+        {
+            Console.WriteLine("Cost (THB): 33");
+        }
+        if (answer == 6 )
+        {
+            Console.WriteLine("Cost (THB): 37");
+        }
+        if (answer == 7 )
+        {
+            Console.WriteLine("Cost (THB): 40");
+        }
+         if ((answer >= 8 )&& (answer <= 15))
+        {
+            Console.WriteLine("Cost (THB): 40");
+        }
+        if(answer > 15)
+        {
+            Console.WriteLine("Cost (THB): 59");
+        }
+
+
+
+    }
+
+}
